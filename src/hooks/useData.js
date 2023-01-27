2499ec952d8e3061c8b94eff0e5dbbd171cb3f0b
Avishekdevnath <avishekdevnath@gmail.com>
2023-01-27 21:07:15 +0600

    Service Page

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -14,25 +14,6 @@ const useData = () =>{
 
 
 
-
-    // const [products, setProducts] = useState([]);
-    // // const [isLoading, setIsLoading ] = useState(true);
-    
-
-    // useEffect(()=>{
-        
-    //     // setIsLoading(true);
-    //     fetch(`https://taja-jinis.herokuapp.com/products`)
-    //     // fetch('http://localhost:5000/products')
-    //     .then(res => res.json())
-    //     .then(data =>{
-    //         setProducts(data);
-    //         // setIsLoading(false);
-    //         // console.log(data);
-    //     });
-    // } , [])
-
-
     return [data, setData];
 }
 export default useData;
